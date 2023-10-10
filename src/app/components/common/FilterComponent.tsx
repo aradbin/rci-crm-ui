2e6bdf00b914cb753a7bcfef6dd84ef21c0f63ec
Arad Bin <aradbin@gmail.com>
2023-10-11 02:20:14 +0600

    Toolbar component update with filter

--- src/app/components/common/FilterComponent.tsx
+++ src/app/components/common/FilterComponent.tsx
@@ -2,6 +2,7 @@ import { Field, FormikProvider, useFormik } from "formik"
 import * as Yup from 'yup'
 import { InputField } from "../fields/InputField"
 import { SelectField } from "../fields/SelectField"
+import { KTIcon } from "../../../_metronic/helpers"
 
 const FilterComponent = ({filter, submit}: any) => {
     const formik = useFormik({
@@ -12,41 +13,46 @@ const FilterComponent = ({filter, submit}: any) => {
     })
 
     return (
-        <div className='menu menu-sub menu-sub-dropdown w-250px w-md-300px' data-kt-menu='true'>
-            <FormikProvider value={formik}>
-                <form className="form" onSubmit={formik.handleSubmit} noValidate>
-                    <div className='px-7 py-5'>
-                        <div className='fs-5 text-dark fw-bolder'>Filter Options</div>
-                    </div>
-                    <div className='separator border-gray-200'></div>
-                    <div className='px-7 py-5'>
-                        {filter?.fields?.map((item: any, index: number) =>
-                            <Field
-                                key={index}
-                                label={item?.label}
-                                name={item?.name}
-                                type={item?.type || "text"}
-                                required={item?.required || ""}
-                                component={item?.type === 'select' ? SelectField : InputField}
-                                options={item?.options}
-                                size={item?.size || "sm"}
-                            />
-                        )}
-                    </div>
-                    <div className='separator border-gray-200'></div>
-                    <div className='d-flex justify-content-end px-7 py-5'>
-                        <button type='reset' className='btn btn-sm btn-light btn-active-light-primary me-2' data-kt-menu-dismiss='true' onClick={() => {
-                            formik.resetForm();
-                            submit({});
-                        }}>
-                            Reset
-                        </button>
-                        <button type='submit' className='btn btn-sm btn-primary' data-kt-menu-dismiss='true'>
-                            Apply
-                        </button>
-                    </div>
-                </form>
-            </FormikProvider>
+        <div className='m-0'>
+            <a href='#' className='btn btn-sm btn-flex fw-bold btn-outline btn-outline-dashed btn-outline-primary' data-kt-menu-trigger='click' data-kt-menu-placement='bottom-end'>
+                <KTIcon iconName='filter' className='fs-6 text-muted me-1' /> Filter
+            </a>
+            <div className='menu menu-sub menu-sub-dropdown w-250px w-md-300px' data-kt-menu='true'>
+                <FormikProvider value={formik}>
+                    <form className="form" onSubmit={formik.handleSubmit} noValidate>
+                        <div className='px-7 py-5'>
+                            <div className='fs-5 text-dark fw-bolder'>Filter Options</div>
+                        </div>
+                        <div className='separator border-gray-200'></div>
+                        <div className='px-7 py-5'>
+                            {filter?.fields?.map((item: any, index: number) =>
+                                <Field
+                                    key={index}
+                                    label={item?.label}
+                                    name={item?.name}
+                                    type={item?.type || "text"}
+                                    required={item?.required || ""}
+                                    component={item?.type === 'select' ? SelectField : InputField}
+                                    options={item?.options}
+                                    size={item?.size || "sm"}
+                                />
+                            )}
+                        </div>
+                        <div className='separator border-gray-200'></div>
+                        <div className='d-flex justify-content-end px-7 py-5'>
+                            <button type='reset' className='btn btn-sm btn-light btn-active-light-primary me-2' data-kt-menu-dismiss='true' onClick={() => {
+                                formik.resetForm();
+                                submit({});
+                            }}>
+                                Reset
+                            </button>
+                            <button type='submit' className='btn btn-sm btn-primary' data-kt-menu-dismiss='true'>
+                                Apply
+                            </button>
+                        </div>
+                    </form>
+                </FormikProvider>
+            </div>
         </div>
     )
 }
--- src/app/components/common/ToolbarComponent.tsx
+++ src/app/components/common/ToolbarComponent.tsx
@@ -7,12 +7,7 @@ const ToolbarComponent = ({title, breadCrumbs, handleButtonClick, children}) => 
             <div id="kt_app_toolbar_container" className='d-flex flex-stack container-fluid p-0'>
                 <PageTitleComponent pageTitle={title} pageBreadcrumbs={breadCrumbs} />
                 <div className='d-flex align-items-center gap-2 gap-lg-3'>
-                    {children && <div className='m-0'>
-                        <a href='#' className='btn btn-sm btn-flex fw-bold bg-body btn-color-gray-700 btn-active-color-primary' data-kt-menu-trigger='click' data-kt-menu-placement='bottom-end'>
-                            <KTIcon iconName='filter' className='fs-6 text-muted me-1' /> Filter
-                        </a>
-                        {children}
-                    </div>}
+                    {children}
                     <button className='btn btn-sm fw-bold btn-primary' onClick={() => handleButtonClick(true)}>
                         <KTIcon iconName='plus' className='fs-3' /> Create
                     </button>
