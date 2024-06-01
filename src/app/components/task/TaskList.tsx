18d265fb6cb711e125a7fe87951a7c93dc6e170e
Arad Bin <aradbin@gmail.com>
2024-06-01 19:01:24 +0600

    task count on list

--- src/app/components/task/TaskList.tsx
+++ src/app/components/task/TaskList.tsx
@@ -5,9 +5,12 @@ import { TASKS_URL } from "../../helpers/ApiEndpoints"
 import { stringifyRequestQuery } from "../../helpers/Utils"
 import { taskColumns } from "../../columns/taskColumns"
 import { AppContext } from "../../providers/AppProvider"
+import { getRequest } from "../../helpers/Requests"
 
 const TaskList = ({ filterParams }: any) => {
     const [params, setParams] = useState<any>({...filterParams})
+    const [loading, setLoading] = useState(false)
+    const [counts, setCounts] = useState([])
     const [status, setStatus] = useState("")
 
     const { refetchTask } = useContext(AppContext)
@@ -18,12 +21,28 @@ const TaskList = ({ filterParams }: any) => {
             formData.status = status
         }
         setParams(formData)
+        setLoading(true)
+        getRequest(`${TASKS_URL}/count`, stringifyRequestQuery(filterParams)).then((response) => {
+            setCounts(response)
+        }).finally(() => {
+            setLoading(false)
+        })
     },[filterParams, status])
+
+    const getCount = (value: string) => {
+        const found: any = counts?.find((count: { status: string, count: string }) => count?.status === value)
+        if(found){
+            return found?.count
+        }
+        return '0'
+    }
 
     return (<>
         <div className="card-header justify-content-end align-items-center gap-2">
             {statuses?.map((item) =>
-                <button key={item?.value} className={`btn btn-sm btn-outline ${item?.value === status ? `btn-${item?.color}` : `btn-outline-${item?.color}`}`} onClick={() => item?.value !== status ? setStatus(item?.value) : setStatus("")}>{item?.label}</button>
+                <button key={item?.value} className={`btn btn-sm btn-outline ${item?.value === status ? `btn-${item?.color}` : `btn-outline-${item?.color}`}`} onClick={() => item?.value !== status ? setStatus(item?.value) : setStatus("")}>
+                    {item?.label}: {loading ? <span className='spinner-border align-middle' style={{ width: '1rem', height: '1rem' }}></span> : <span>{getCount(item?.value)}</span>}
+                </button>
             )}
         </div>
         <div className='card-body py-3'>
