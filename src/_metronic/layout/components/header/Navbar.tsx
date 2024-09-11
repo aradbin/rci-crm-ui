f8014cc31f5dc2f08d8d1bab66ca0a0518ba4ec4
Arad bin <arad.siddiqui@upaybd.com>
2024-09-11 16:24:04 +0600

    navbar search revert

--- src/_metronic/layout/components/header/Navbar.tsx
+++ src/_metronic/layout/components/header/Navbar.tsx
@@ -15,9 +15,9 @@ const Navbar = () => {
   const {currentUser} = useAuth()
   return (
     <div className='app-navbar flex-shrink-0'>
-      <div className={clsx('app-navbar-item align-items-stretch', itemClass)}>
+      {/* <div className={clsx('app-navbar-item align-items-stretch', itemClass)}>
         <Search />
-      </div>
+      </div> */}
 
       {/* <div className={clsx('app-navbar-item', itemClass)}>
         <div id='kt_activities_toggle' className={btnClass}>
