5b4edc58083f47d01d46e229179b4ba60ccc6480
Arad bin <arad.siddiqui@upaybd.com>
2024-11-04 17:57:14 +0600

    whatsapp search update

--- src/app/components/whatsapp/ChatBox.tsx
+++ src/app/components/whatsapp/ChatBox.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import { useEffect, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { CHAT_ATTENDEES_UNIPILE_URL, CHATS_UNIPILE_URL } from '../../helpers/ApiEndpoints'
 import { QueryInfiniteUnipile } from '../../helpers/Queries'
 import { ChatInner } from './ChatInner'
@@ -7,6 +7,7 @@ import { formatDate, getSettingsFromUserSettings } from '../../helpers/Utils'
 import { LoadingComponent } from '../common/LoadingComponent'
 import { useAuth } from '../../modules/auth'
 import { AvatarComponent } from '../common/AvatarComponent'
+import { AppContext } from '../../providers/AppProvider'
 
 const ChatBox = () => {
   const { currentUser } = useAuth()
@@ -14,6 +15,7 @@ const ChatBox = () => {
   const [account, setAccount] = useState(getSettingsFromUserSettings(currentUser?.userSettings, 'whatsapp')[0]?.unipile_account_id)
   const [conversations, setConversations] = useState<any>([])
   const [selectedConversation, setSelectedConversation]: any = useState()
+  const { customers, contacts } = useContext(AppContext)
 
   let { data, fetchNextPage, hasNextPage, isFetchingNextPage } = QueryInfiniteUnipile(`all-whatsapp-${account}`, CHATS_UNIPILE_URL, { account_id: account, limit: 250 }, account ? true : false)
 
@@ -35,10 +37,14 @@ const ChatBox = () => {
       data?.pages?.forEach((page: any) => {
         const items: any = []
         page?.items?.forEach((item: any) => {
+          const customer = customers?.find((c: any) => c?.contact && item?.provider_id?.includes(c?.contact))
+          const contact = contacts?.find((c: any) => c?.contact && item?.provider_id?.includes(c?.contact))
           const attendee = allAttendees.find((a: any) => a?.provider_id === item?.provider_id)
           items.push({
             ...item,
-            attendee: attendee
+            attendee: attendee,
+            customer: customer,
+            contact: contact
           })            
         });
         pages.push({
@@ -97,16 +103,35 @@ const ChatBox = () => {
               {conversations?.pages?.map((page: any, index: number) => (
                 <div key={index}>
                   {page?.items?.map((item: any) => {
-                    if(item?.provider_id !== 'status@broadcast' && (!filter || item?.provider_id?.toLowerCase()?.includes(filter?.toLowerCase()) || item?.name?.toLowerCase()?.includes(filter?.toLowerCase()) || item?.attendee?.name?.toLowerCase()?.includes(filter?.toLowerCase()))){
+                    if(item?.provider_id !== 'status@broadcast' && (
+                      !filter 
+                      || item?.provider_id?.toLowerCase()?.includes(filter?.toLowerCase()) 
+                      || item?.name?.toLowerCase()?.includes(filter?.toLowerCase()) 
+                      || item?.attendee?.name?.toLowerCase()?.includes(filter?.toLowerCase())
+                      || item?.customer?.name?.toLowerCase()?.includes(filter?.toLowerCase())
+                      || item?.contact?.name?.toLowerCase()?.includes(filter?.toLowerCase())
+                      || item?.customer?.contact?.toLowerCase()?.includes(filter?.toLowerCase())
+                      || item?.contact?.contact?.toLowerCase()?.includes(filter?.toLowerCase())
+                    )){
+                      let name = item?.name
+                      if(item?.attendee){
+                        name = item?.attendee?.name
+                      }
+                      if(item?.customer){
+                        name = (name && name !== '') ? `${name} (${item?.customer?.name})` : item?.customer?.name
+                      }
+                      if(item?.contact){
+                        name = (name && name !== '') ? `${name} (${item?.contact?.name})` : item?.contact?.name
+                      }
                       return (
-                        <div className={`d-flex flex-stack pe-5 ps-2 py-3 cursor-pointer rounded ${selectedConversation?.id === item?.id ? 'bg-success-subtle' : ''}`} key={item?.id} onClick={() => setSelectedConversation(item)}>
+                        <div className={`d-flex flex-stack pe-5 ps-2 py-3 cursor-pointer rounded ${selectedConversation?.id === item?.id ? 'bg-success-subtle' : ''}`} key={item?.id} onClick={() => setSelectedConversation({...item, name: name})}>
                           <div className='d-flex align-items-center'>
-                            <AvatarComponent avatar={item?.customer?.avatar} name={item?.name || item?.attendee?.name || item?.provider_id?.split('@')[0]} style='circle' size='40' />
+                            <AvatarComponent avatar={item?.customer?.avatar} name={name || item?.provider_id?.split('@')[0]} style='circle' size='40' />
                             <div className='ms-5'>
-                              <span className='fs-5 fw-bolder text-gray-900 text-hover-primary mb-2'>
-                                {item?.name || item?.attendee?.name?.split('@')[0] || item?.provider_id?.split('@')[0]}
+                              <span className='fs-5 fw-bolder text-gray-900 text-hover-primary mb-2' style={{ wordBreak: 'break-all' }}>
+                                {name || item?.provider_id?.split('@')[0]}
                               </span>
-                              {(item?.name || item?.attendee?.name) && <div className='fw-bold text-gray-400'>{item?.provider_id?.split('@')[0]}</div>}
+                              {name && <div className='fw-bold text-gray-400'>{item?.provider_id?.split('@')[0]}</div>}
                             </div>
                             {item?.unread === 1 &&item?.unread_count > 0 && <span className="badge badge-circle badge-light-success ms-3">{item?.unread_count}</span>}
                           </div>
